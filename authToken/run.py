e7a55ce87612ac3b6685139c158a98271057ceb1
hieu1997 <hieu.leminh@gmail.com>
2019-06-04 17:34:08 +0700

    update

--- authToken/run.py
+++ authToken/run.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-
+from flask_cors import CORS
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -36,3 +37,6 @@ api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
 api.add_resource(resources.TokenRefresh, '/token/refresh')
 api.add_resource(resources.AllUsers, '/users')
 api.add_resource(resources.SecretResource, '/secret')
+
+if __name__ == "__main__":
+    app.run(debug=True,host = '0.0.0.0',port = 5001)
